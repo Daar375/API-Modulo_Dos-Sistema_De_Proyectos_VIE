7960cddd181c5b5329540107b133b76cfa2bb1d8
Daar375 <Daar375@gmail.com>
2021-10-05 16:17:10 -0600

    Tipo Informe y sub area controllers

---
+++ modulo2DBConectionAPI/modulo2DBConectionAPI/Controllers/TipoInformeController.cs
@@ -0,0 +1,35 @@
+﻿using AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace modulo2DBConectionAPI.Controllers
+{
+    public class TipoInformeController : ApiController
+    {
+
+        public List<TipoInforme> Get()
+        {
+            using (VieEntidades entidades = new VieEntidades())
+            {
+                entidades.Configuration.ProxyCreationEnabled = false;
+                return entidades.TipoInforme.ToList();
+            }
+        }
+
+
+        /*    [HttpPost]
+            public List<Pr_ProyectoXFrascati_Insertar_Result> Post(string codigoProyecto, int area, int subArea)
+            {
+                using (VieEntidades entidades = new VieEntidades())
+                {
+                    entidades.Configuration.ProxyCreationEnabled = false;
+                    System.Data.Objects.ObjectResult<Pr_ProyectoXFrascati_Insertar_Result> res = entidades.Pr_ProyectoXFrascati_Insertar(codigoProyecto, subArea, area);
+                    return res.ToList();
+                }
+            }*/
+    }
+}
