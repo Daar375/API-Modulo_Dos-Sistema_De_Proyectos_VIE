ef2bd32b012dec1bffcc73252c0f5a9d06f90bbf
Daar375 <Daar375@gmail.com>
2021-10-05 08:40:28 -0600

    Agregado tipo de departamento

---
+++ modulo2DBConectionAPI/modulo2DBConectionAPI/Controllers/TipoDepartamentoController.cs
@@ -0,0 +1,23 @@
+﻿using AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace modulo2DBConectionAPI.Controllers
+{
+    public class TipoDepartamentoController : ApiController
+    {
+        // GET: TipoDepartamento
+        public List<TipoDepartamento> Get()
+        {
+            using (VieEntidades entities = new VieEntidades())
+            {
+                entities.Configuration.ProxyCreationEnabled = false;
+                return entities.TipoDepartamento.ToList();
+            }
+        }
+    }
+}
