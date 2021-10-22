04b5f9f4687cdcec07e6b23ac0aae82638d2ef51
Daar375 <Daar375@gmail.com>
2021-10-22 14:32:43 -0600

    Cambiar coordinador

---
+++ modulo2DBConectionAPI/modulo2DBConectionAPI/Controllers/CoordinadorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AccesoDatos;
+
+namespace modulo2DBConectionAPI.Controllers
+{
+    public class CoordinadorController : ApiController
+    {
+
+        public System.Data.Objects.ObjectResult<int?> Post(string codigo, string numIdentidad)
+        {
+            using (VieEntidades entities = new VieEntidades())
+            {
+                entities.Configuration.ProxyCreationEnabled = false;
+
+                return entities.Pr_Proyecto_CambiarInvCoord(codigo, numIdentidad);
+            }
+
+        }
+
+
+    }
+}
