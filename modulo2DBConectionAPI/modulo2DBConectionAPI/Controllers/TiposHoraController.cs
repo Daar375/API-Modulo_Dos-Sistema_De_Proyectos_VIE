71573646cf6f7eb7b1f16bfb9a4dd49cbcf88f00
Daar375 <Daar375@gmail.com>
2021-10-23 17:49:20 -0600

    Tipos hora

---
+++ modulo2DBConectionAPI/modulo2DBConectionAPI/Controllers/TiposHoraController.cs
@@ -0,0 +1,26 @@
+﻿using AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace modulo2DBConectionAPI.Controllers
+{
+    public class TipoHoraController : ApiController
+    {
+
+        public List<TipoHora> Get()
+        {
+            using (VieEntidades entidades = new VieEntidades())
+            {
+                entidades.Configuration.ProxyCreationEnabled = false;
+                return entidades.TipoHora.ToList();
+            }
+        }
+
+
+
+    }
+}
