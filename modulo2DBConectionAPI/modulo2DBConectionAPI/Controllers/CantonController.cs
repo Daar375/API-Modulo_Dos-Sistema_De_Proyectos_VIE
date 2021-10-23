8201707555a05dcd6ebe5c5bb3114ed016a2fb72
Daar375 <Daar375@gmail.com>
2021-10-23 17:31:45 -0600

    Pais provincia canton

---
+++ modulo2DBConectionAPI/modulo2DBConectionAPI/Controllers/CantonController.cs
@@ -0,0 +1,22 @@
+﻿using AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace modulo2DBConectionAPI.Controllers
+{
+    public class CatonController : ApiController
+    {
+        public List<Canton> Get()
+        {
+            using (VieEntidades entities = new VieEntidades())
+            {
+                entities.Configuration.ProxyCreationEnabled = false;
+                return entities.Canton.ToList();
+            }
+        }
+    }
+}
---
+++ modulo2DBConectionAPI/modulo2DBConectionAPI/Controllers/PaisController.cs
@@ -0,0 +1,22 @@
+﻿using AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace modulo2DBConectionAPI.Controllers
+{
+    public class PaisController : ApiController
+    {
+        public List<Pais> Get()
+        {
+            using (VieEntidades entities = new VieEntidades())
+            {
+                entities.Configuration.ProxyCreationEnabled = false;
+                return entities.Pais.ToList();
+            }
+        }
+    }
+}
